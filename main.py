8df354879c714fb741b6e49e14ab9e8cc0ce90e5
Shail Choksi <shail@pronavigator.io>
2018-04-16 11:09:26 -0400

    Cleanup and refactor

--- main.py
+++ main.py
@@ -6,20 +6,40 @@ import json
 import logging
 
 
-def start(token, game_id, engine, weights=None):
-    # init
+def is_bot_account(token):
     li = lichess.Lichess(token)
+    user_profile = li.get_profile()
+    return user_profile.get("bot") is not None
+
+
+def upgrade_account(li):
+    if li.upgrade_to_bot_account() is None:
+        return False
+
+    print("Succesfully upgraded {} to Bot Account!".format(username))
+    return True
+
+
+def start(li, game_id, engine, weights=None):
+    # init
+    user_profile = li.get_profile()
+    username = user_profile.get("username")
+    print("Welcome {}!".format(username))
+
     stream = li.get_stream(game_id)
     updates = stream.iter_lines()
-    #Lichess Game info
+
+    #Initial response of stream will be the full game info. Store it
     game_info = json.loads(next(updates).decode('utf-8'))
     board = setup_board(game_info)
     engine = setup_engine(engine, board, weights)
 
-    username = li.get_profile()["username"]
     # need to do this to check if its playing against SF.
+    # If Lichess Stockfish is playing response will contain:
+    # 'white':{'aiLevel': 6} or 'black':{'aiLevel': 6}
+    # instead of user info
     is_white = False
-    if game_info.get("white"):
+    if game_info.get("white").get("name"):
         is_white = (game_info.get("white")["name"] == username)
 
     print(game_info)
@@ -104,13 +124,24 @@ def get_time_controls(data):
 
     return wtime, btime, winc, binc
 
+
 if __name__ == "__main__":
     logger = logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description='Play on Lichess with a bot')
     parser.add_argument('token', type=str, help='OAuth token')
-    parser.add_argument('gameid', type=str, help='Lichess Game Id')
-    parser.add_argument('engine', type=str, help='path to engine binary')
-    parser.add_argument('--weights', type=str, help='path to training data (weights) file')
+    parser.add_argument('-g', '--gameid', type=str, help='Lichess Game Id')
+    parser.add_argument('-e', '--engine', type=str, help='path to engine binary')
+    parser.add_argument('-w', '--weights', type=str, help='path to training data (weights) file')
+    parser.add_argument('-u', action='store_true', help='Add this flag to upgrade your account to a bot account.')
     args = parser.parse_args()
 
-    start(args.token, args.gameid, args.engine, args.weights)
+    li = lichess.Lichess(args.token)
+
+    is_bot = is_user_bot(li)
+    if args.u is True and is_bot is False:
+        is_bot = upgrade_account(li)
+
+    if is_bot:
+        start(li, args.gameid, args.engine, args.weights)
+    else:
+        print("This is not a bot account. Please upgrade your Lichess account to a bot account!")

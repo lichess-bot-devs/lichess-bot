ce257a66942d5fbe890df4ecb459bc3d922f7ad8
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-23 17:40:01 +0200

    bump 0.8

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ try:
 except ImportError:
     from http.client import BadStatusLine as RemoteDisconnected
 
-__version__ = "0.7"
+__version__ = "0.8"
 
 def upgrade_account(li):
     if li.upgrade_to_bot_account() is None:

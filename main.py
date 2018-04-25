fd85ebcaa9c6e943abce5732206e7098ad9b65aa
Shail Choksi <choksishail@gmail.com>
2018-04-25 16:28:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ try:
 except ImportError:
     from http.client import BadStatusLine as RemoteDisconnected
 
-__version__ = "0.9"
+__version__ = "1.0"
 
 def upgrade_account(li):
     if li.upgrade_to_bot_account() is None:

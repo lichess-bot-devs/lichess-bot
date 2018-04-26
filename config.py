b7a406d0face31429d6e0efc7aaf7980a94c7bf2
Tony <219885+tttony@users.noreply.github.com>
2018-04-26 17:51:29 -0400

    Added options to select polyglot best move (update with the new commit)

--- config.py
+++ config.py
@@ -11,15 +11,15 @@ def load_config():
             raise e
 
         #[section, type, error message]
-        sections = [["token", str, "Section `token` must be a string wrapped in quotes."],
-                    ["url", str, "Section `url` must be a string wrapped in quotes."],
-                    ["engine", dict, "Section `engine` must be a dictionary with indented keys followed by colons.."],
-                    ["max_concurrent_games", int, "Section `max_concurrent_games` must be an integer number without quotes."],
-                    ["supported_tc", list, "Section `supported_tc` must be a list with indented entries starting with dashes.."],
-                    ["supported_modes", list, "Section `supported_modes` must be a list with indented entries starting with dashes.."]]
+        sections = [["token", str, "Section ´token´ must be a string wrapped in quotes."],
+                    ["url", str, "Section ´url´ must be a string wrapped in quotes."],
+                    ["engine", dict, "Section ´engine´ must be a dictionary with indented keys followed by colons.."],
+                    ["max_concurrent_games", int, "Section ´max_concurrent_games´ must be an integer number without quotes."],
+                    ["supported_tc", list, "Section ´supported_tc´ must be a list with indented entries starting with dashes.."],
+                    ["supported_modes", list, "Section ´supported_modes´ must be a list with indented entries starting with dashes.."]]
         for section in sections:
             if section[0] not in CONFIG:
-                raise Exception("Your config.yml does not have required section `{}`.".format(section[0]))
+                raise Exception("Your config.yml does not have required section ´{}´.".format(section[0]))
             elif not isinstance(CONFIG[section[0]], section[1]):
                 raise Exception(section[2])
 
@@ -27,12 +27,12 @@ def load_config():
                            ["name", str, "´name´ must be a string wrapped in quotes."],
                            ["polyglot", bool, "´polyglot´ must be a boolean type: true or false."],
                            ["polyglot_book", str, "´polyglot_book´ must be a string wrapped in quotes."],
-                           ["polyglot_max_depth", int, "`polyglot_max_depth` must be an integer number without quotes."],
-                           ["polyglot_min_weight", int, "`polyglot_min_weight` must be an integer number without quotes."],
-                           ["polyglot_random", bool, "`polyglot_random` must be a boolean type: true or false."]]
+                           ["polyglot_max_depth", int, "´polyglot_max_depth´ must be an integer number without quotes."],
+                           ["polyglot_min_weight", int, "´polyglot_min_weight´ must be an integer number without quotes."],
+                           ["polyglot_move_selection", str, "´polyglot_move_selection´ must be a string wrapped in quotes."]]
         for subsection in engine_sections:
             if subsection[0] not in CONFIG["engine"]:
-                raise Exception("Your config.yml does not have required `engine` subsection `{}`.".format(subsection))
+                raise Exception("Your config.yml does not have required ´engine´ subsection ´{}´.".format(subsection))
             if not isinstance(CONFIG["engine"][subsection[0]], subsection[1]):
                 raise Exception("´engine´ subsection {}".format(subsection[2]))
 
@@ -40,7 +40,7 @@ def load_config():
             raise Exception("Your config.yml has the default Lichess API token. This is probably wrong.")
 
         if not os.path.isdir(CONFIG["engine"]["dir"]):
-            raise Exception("Your engine directory `{}` is not a directory.")
+            raise Exception("Your engine directory ´{}´ is not a directory.")
 
         engine = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["name"])
 
@@ -50,7 +50,11 @@ def load_config():
         if not os.access(engine, os.X_OK):
             raise Exception("The engine %s doesn't have execute (x) permission. Try: chmod +x %s" % (engine, engine))
 
-        if (CONFIG["engine"]["polyglot"] == True):
+        if CONFIG["engine"]["polyglot"] == True:
+            polyglot_move_values = ["random_move", "random_weight", "best_move"]
+            if CONFIG["engine"]["polyglot_move_selection"] not in polyglot_move_values:
+                raise Exception("´polyglot_move_selection´ must be one of these values: %s" % ", ".join(polyglot_move_values))
+
             book_dir = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["polyglot_book"])
             if not os.path.isfile(book_dir):
                 raise Exception("The polyglot book %s file does not exist." % book_dir)

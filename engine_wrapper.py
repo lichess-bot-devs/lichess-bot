3cf75614886d32cc5183528b4202f2d057421091
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-05-03 16:36:00 +0200

    relocate engine.xboardoptions

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -29,7 +29,7 @@ def create_engine(config, board):
     silence_stderr = cfg.get("silence_stderr", False)
 
     if engine_type == "xboard":
-        return XBoardEngine(board, commands, config.get("xboardoptions"), silence_stderr)
+        return XBoardEngine(board, commands, cfg.get("xboardoptions"), silence_stderr)
 
     return UCIEngine(board, commands, cfg.get("ucioptions"), silence_stderr)
 

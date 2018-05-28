0ccc37f705cc78d31992fb9c68a575041d9cdfa5
Shail Choksi <shail@pronavigator.io>
2018-05-28 13:53:57 -0400

    Create new list for stats in conversation

--- conversation.py
+++ conversation.py
@@ -38,8 +38,8 @@ class Conversation():
         elif cmd == "eval" and line.room == "spectator":
             stats = self.engine.get_stats()
             num_moves = int(len(game.state["moves"].split())/2.0) + 1
-            stats.append("move: {}".format(num_moves))
-            self.send_reply(line, ", ".join(stats))
+            myStats = stats + ["move: {}".format(num_moves))]
+            self.send_reply(line, ", ".join(myStats))
         elif cmd == "eval":
             self.send_reply(line, "I don't tell that to my opponent, sorry.")
         elif cmd == "id":

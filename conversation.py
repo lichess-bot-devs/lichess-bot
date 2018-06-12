f2b891fb4ebe10d87c5569e2854c52e328ebe6b8
Shail Choksi <shail@pronavigator.io>
2018-06-11 23:36:49 -0400

    Bump to 395

--- conversation.py
+++ conversation.py
@@ -6,7 +6,7 @@ LINKS = {
     "Lichess Bots": "https://lichess.org/api#tag/Chess-Bot"
 }
 
-ID = 361
+ID = 395
 
 class Conversation():
     def __init__(self, game, engine, xhr, version, challenge_queue):

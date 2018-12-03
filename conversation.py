c8da778312ce18384b6b3787f8b1ce037ec073a0
QueensGambit <curry-berry@freenet.de>
2018-12-03 19:56:21 +0100

    changed 'lichess-bot' to 'lichessbot' because '-' couldn't be understood properly

--- conversation.py
+++ conversation.py
@@ -20,7 +20,7 @@ class Conversation():
             game.abort_in(60)
             self.send_reply(line, "Waiting 60 seconds...")
         elif cmd == "name":
-            self.send_reply(line, "{} (lichess-bot v{})".format(self.engine.name(), self.version))
+            self.send_reply(line, "{} (lichessbot v{})".format(self.engine.name(), self.version))
         elif cmd == "howto":
             self.send_reply(line, "How to run your own bot: lichess.org/api#tag/Chess-Bot")
         elif cmd == "eval" and line.room == "spectator":

4e8ffe39ab6c6f9dccbef63ebf1d776ca1600109
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-06-26 21:48:19 +0100

    Wrong directory doesnt actually display directory

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ def load_config(config_file):
             raise Exception("Your config.yml has the default Lichess API token. This is probably wrong.")
 
         if not os.path.isdir(CONFIG["engine"]["dir"]):
-            raise Exception("Your engine directory `{}` is not a directory.")
+            raise Exception("Your engine directory `{}` is not a directory.".format(CONFIG["engine"]["dir"]))
 
         engine = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["name"])
 

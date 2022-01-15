9633e0b926f8b76d7a731eac989d6a83d45df0ee
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-01-15 09:47:22 +0200

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -45,8 +45,8 @@ class MinimalEngine(EngineWrapper):
     however you can also change other methods like
     `notify`, `first_search`, `get_time_control`, etc.
     """
-    def __init__(self, commands, options, stderr, name=None):
-        super().__init__(commands, options, stderr)
+    def __init__(self, commands, options, stderr, draw_or_resign, name=None):
+        super().__init__(commands, options, stderr, draw_or_resign)
         self.go_commands = options.pop("go_commands", {}) or {}
 
         self.engine_name = self.__class__.__name__ if name is None else name

f2a5aec4a0aa17e0a71b75a8baef758492549df5
Mark Harrison <mark.z.harrison@gmail.com>
2022-01-28 00:12:46 -0800

    Get last message after game clock expires
    If a bot runs out of time, this change will make sure that the last message
    from lichess is received. Prior to this change, the user would only see
    an api_post() error and the game would end.

--- lichess-bot.py
+++ lichess-bot.py
@@ -266,6 +266,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     first_move = True
     correspondence_disconnect_time = 0
     while not terminated:
+        move_attempted = False
         try:
             if first_move:
                 upd = game.state
@@ -301,6 +302,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
                             best_move = choose_move_time(engine, board, correspondence_move_time, can_ponder, draw_offered)
                         else:
                             best_move = choose_move(engine, board, game, can_ponder, draw_offered, start_time, move_overhead)
+                    move_attempted = True
                     if best_move.resigned and len(board.move_stack) >= 2:
                         li.resign(game.id)
                     else:
@@ -327,6 +329,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
                         li.abort(game.id)
                     break
         except (HTTPError, ReadTimeout, RemoteDisconnected, ChunkedEncodingError, ConnectionError, ProtocolError):
+            if move_attempted:
+                continue
             if game.id not in (ongoing_game["gameId"] for ongoing_game in li.get_ongoing_games()):
                 break
         except StopIteration:

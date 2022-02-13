7c915625f99c3bdb0e0e556ff0fe8972426b9c3c
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-02-13 21:49:39 +0200

    Use username instead of user_profile["username"]

--- lichess-bot.py
+++ lichess-bot.py
@@ -711,4 +711,4 @@ if __name__ == "__main__":
         engine_factory = partial(engine_wrapper.create_engine, CONFIG)
         start(li, user_profile, engine_factory, CONFIG, logging_level, args.logfile)
     else:
-        logger.error(f'{user_profile["username"]} is not a bot account. Please upgrade it to a bot account!')
+        logger.error(f"{username} is not a bot account. Please upgrade it to a bot account!")

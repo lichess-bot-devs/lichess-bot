3dc21ec4d998c827596391fce4a9c97fc5752864
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-03-04 15:58:02 +0530

    Update engine_wrapper.py

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -11,8 +11,7 @@ logger = logging.getLogger(__name__)
 @backoff.on_exception(backoff.expo, BaseException, max_time=120)
 def create_engine(config, variant):
     cfg = config["engine"]
-    normal_chess = variant.lower() in ["standard", "from position"]
-    engine_path = os.path.join(cfg["dir"], cfg["name"] if normal_chess else cfg.get(f"{variant}name") or cfg["name"])
+    engine_path = os.path.join(cfg["dir"], cfg.get(f"{variant}name") or cfg["name"])
     engine_working_dir = cfg.get("working_dir") or os.getcwd()
     engine_type = cfg.get("protocol")
     engine_options = cfg.get("engine_options")

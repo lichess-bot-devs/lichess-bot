fd6f75e39364e1b707a193144f8379308a9e55b6
Mark Harrison <mark.z.harrison@gmail.com>
2022-08-04 22:00:01 +0800

    Allow bot account to query !eval

--- conversation.py
+++ conversation.py
@@ -14,12 +14,12 @@ class Conversation:
     command_prefix = "!"
 
     def react(self, line, game):
-        if line.username != self.game.username:
-            logger.info(f'*** {self.game.url()} [{line.room}] {line.username}: {line.text.encode("utf-8")}')
-            if line.text[0] == self.command_prefix:
-                self.command(line, game, line.text[1:].lower())
+        logger.info(f'*** {self.game.url()} [{line.room}] {line.username}: {line.text.encode("utf-8")}')
+        if line.text[0] == self.command_prefix:
+            self.command(line, game, line.text[1:].lower())
 
     def command(self, line, game, cmd):
+        from_self = line.username == self.game.username
         if cmd == "commands" or cmd == "help":
             self.send_reply(line, "Supported commands: !wait (wait a minute for my first move), !name, !howto, !eval, !queue")
         elif cmd == "wait" and game.is_abortable():
@@ -30,7 +30,7 @@ class Conversation:
             self.send_reply(line, f"{name} running {self.engine.name()} (lichess-bot v{self.version})")
         elif cmd == "howto":
             self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
-        elif cmd == "eval" and line.room == "spectator":
+        elif cmd == "eval" and (from_self or line.room == "spectator"):
             stats = self.engine.get_stats(for_chat=True)
             self.send_reply(line, ", ".join(stats))
         elif cmd == "eval":

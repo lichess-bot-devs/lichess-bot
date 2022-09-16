27de77b12713282a4f72534676ef81501938a52b
Mark Harrison <mark.z.harrison@gmail.com>
2022-09-16 20:43:19 +0800

    Revert "Set delay on usernames only"
    This reverts commit f8f8bbdd9084b371de018e9d19bec0f09ef89d3c.

--- matchmaking.py
+++ matchmaking.py
@@ -114,7 +114,7 @@ class Matchmaking:
         online_bots = list(filter(is_suitable_opponent, online_bots))
 
         def ready_for_challenge(bot):
-            return self.delay_timers[bot["username"]].is_expired()
+            return self.get_delay_timer(bot["username"], variant, game_type, mode).is_expired()
 
         ready_bots = list(filter(ready_for_challenge, online_bots))
         if ready_bots:
@@ -158,11 +158,19 @@ class Matchmaking:
             return
 
         # Add one hour to delay each time a challenge is declined.
-        delay_timer = self.delay_timers[opponent]
+        mode = "rated" if challenge.rated else "casual"
+        delay_timer = self.get_delay_timer(opponent,
+                                           challenge.variant,
+                                           challenge.speed,
+                                           mode)
         delay_timer.duration += 3600
         delay_timer.reset()
         hours = "hours" if delay_timer.duration > 3600 else "hour"
-        logger.info(f"Will not challenge {opponent} for {int(delay_timer.duration/3600)} {hours}.")
+        logger.info(f"Will not challenge {opponent} to a {mode} {challenge.speed} "
+                    f"{challenge.variant} game for {int(delay_timer.duration/3600)} {hours}.")
+
+    def get_delay_timer(self, opponent_name, variant, time_control, rated_mode):
+        return self.delay_timers[(opponent_name, variant, time_control, rated_mode)]
 
 
 def game_category(variant, base_time, increment, days):

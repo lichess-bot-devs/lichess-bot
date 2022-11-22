1135cfce6189be793fc39c0430f7b9df289f123b
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-11-22 15:05:58 +0200

    Update version to 2022.11.22.1

--- lichess-bot.py
+++ lichess-bot.py
@@ -27,7 +27,7 @@ from http.client import RemoteDisconnected
 
 logger = logging.getLogger(__name__)
 
-__version__ = "2022.11.07.1"
+__version__ = "2022.11.22.1"
 
 terminated = False
 restart = True

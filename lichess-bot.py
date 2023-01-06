5b2616df667518a114242387987250ca9f0a996a
Mark Harrison <mark.z.harrison@gmail.com>
2023-01-05 22:04:35 -0800

    Split long exception list

--- lichess-bot.py
+++ lichess-bot.py
@@ -498,7 +498,13 @@ def play_game(li,
                 prior_game = copy.deepcopy(game)
             elif u_type == "ping" and should_exit_game(board, game, prior_game, li, is_correspondence):
                 break
-        except (HTTPError, ReadTimeout, RemoteDisconnected, ChunkedEncodingError, ConnectionError, StopIteration, MoveTimeout) as e:
+        except (HTTPError,
+                ReadTimeout,
+                RemoteDisconnected,
+                ChunkedEncodingError,
+                ConnectionError,
+                StopIteration,
+                MoveTimeout) as e:
             stopped = isinstance(e, StopIteration)
             is_ongoing = game.id in (ongoing_game["gameId"] for ongoing_game in li.get_ongoing_games())
             if stopped or (not move_attempted and not is_ongoing):

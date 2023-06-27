620a87c3befc7cad966546740f9c378a121e762c
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2023-06-27 12:47:52 +0300

    flake8 docstring

--- test_bot/test_bot.py
+++ test_bot/test_bot.py
@@ -26,7 +26,7 @@ stockfish_path = f"./TEMP/sf{file_extension}"
 
 
 def download_sf() -> None:
-    """Download Stockfish 15"""
+    """Download Stockfish 15."""
     windows_or_linux = "win" if platform == "win32" else "linux"
     base_name = f"stockfish_15_{windows_or_linux}_x64"
     exec_name = "stockfish_15_x64"

6235f1da4effa85d321b5ec2ae6acc1819901c84
Mark Harrison <mark.z.harrison@gmail.com>
2023-08-17 19:45:22 -0700

    Fix override parameters that need to be lists

--- config.py
+++ config.py
@@ -231,6 +231,11 @@ def insert_default_values(CONFIG: CONFIG_DICT_TYPE) -> None:
     set_config_default(CONFIG, "matchmaking", key="challenge_variant", default="random")
     set_config_default(CONFIG, "matchmaking", key="challenge_mode", default="random")
     set_config_default(CONFIG, "matchmaking", key="overrides", default={}, force_empty_values=True)
+    for override_config in CONFIG["matchmaking"]["overrides"].values():
+        for parameter in ["challenge_initial_time", "challenge_increment", "challenge_days"]:
+            if parameter in override_config:
+                set_config_default(override_config, key=parameter, default=[None], force_empty_values=True)
+                change_value_to_list(override_config, key=parameter)    
 
     for section in ["engine", "correspondence"]:
         for ponder in ["ponder", "uci_ponder"]:

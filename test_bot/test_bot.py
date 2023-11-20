fb84ff506f682efbb6db983a65c1a2e1e0858861
Mark Harrison <mark.z.harrison@gmail.com>
2023-11-19 18:06:20 -0800

    Consolidate similar code in download_sf()

--- test_bot/test_bot.py
+++ test_bot/test_bot.py
@@ -31,25 +31,25 @@ def download_sf() -> None:
     """Download Stockfish 15."""
     if os.path.exists(stockfish_path):
         return
+
     windows_or_linux = "windows" if platform == "win32" else "ubuntu"
-    if windows_or_linux == "windows":
-        archive_link = ("https://github.com/official-stockfish/Stockfish/releases/download/sf_16/"
-                        "stockfish-windows-x86-64-modern.zip")
-        response = requests.get(archive_link, allow_redirects=True)
-        with open("./TEMP/sf_zip.zip", "wb") as file:
-            file.write(response.content)
-        with zipfile.ZipFile("./TEMP/sf_zip.zip", "r") as archive_ref:
-            archive_ref.extractall("./TEMP/")
-        shutil.copyfile("./TEMP/stockfish/stockfish-windows-x86-64-modern.exe", stockfish_path)
-    else:
-        archive_link = ("https://github.com/official-stockfish/Stockfish/releases/download/sf_16/"
-                        "stockfish-ubuntu-x86-64-modern.tar")
-        response = requests.get(archive_link, allow_redirects=True)
-        with open("./TEMP/sf_zip.tar", "wb") as file:
-            file.write(response.content)
-        with tarfile.TarFile("./TEMP/sf_zip.tar", "r") as archive_ref:
-            archive_ref.extractall("./TEMP/")
-        shutil.copyfile("./TEMP/stockfish/stockfish-ubuntu-x86-64-modern", stockfish_path)
+    sf_base = f"stockfish-{windows_or_linux}-x86-64-modern"
+    archive_ext = "zip" if platform == "win32" else "tar"
+    archive_link = f"https://github.com/official-stockfish/Stockfish/releases/download/sf_16/{sf_base}.{archive_ext}"
+
+    response = requests.get(archive_link, allow_redirects=True)
+    archive_name = f"./TEMP/sf_zip.{archive_ext}"
+    with open(archive_name, "wb") as file:
+        file.write(response.content)
+
+    archive_open = zipfile.ZipFile if archive_ext == "zip" else tarfile.TarFile
+    with archive_open(archive_name, "r") as archive_ref:
+        archive_ref.extractall("./TEMP/")
+
+    exe_ext = ".exe" if platform == "win32" else ""
+    shutil.copyfile(f"./TEMP/stockfish/{sf_base}{exe_ext}", stockfish_path)
+
+    if windows_or_linux == "ubuntu":
         st = os.stat(stockfish_path)
         os.chmod(stockfish_path, st.st_mode | stat.S_IEXEC)
 

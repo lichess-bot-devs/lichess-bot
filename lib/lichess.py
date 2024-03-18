4ed8c35c57e05528e5737d809a5a1b07e44270f0
Greg Finley <gregory.finley@gmail.com>
2024-03-17 23:15:14 -0700

    Remove comment about aligning types

--- lib/lichess.py
+++ lib/lichess.py
@@ -296,12 +296,10 @@ class Lichess:
         """Aborts a game."""
         return cast(OKResponse, self.api_post("abort", game_id))
 
-    # TODO: Align types with test_bot/lichess.py
     def get_event_stream(self) -> requests.models.Response:
         """Get a stream of the events (e.g. challenge, gameStart)."""
         return self.api_get("stream_event", stream=True, timeout=15)
 
-    # TODO: Align types with test_bot/lichess.py
     def get_game_stream(self, game_id: str) -> requests.models.Response:
         """Get  stream of the in-game events (e.g. moves by the opponent)."""
         return self.api_get("stream", game_id, stream=True, timeout=15)

0a8d8df801767cdbac73c8e2a7347dba63c7e2fd
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2024-05-08 14:46:12 +0300

    Update copy_files.sh

--- docker/copy_files.sh
+++ docker/copy_files.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ln -s /lichess-bot/config/config.yml lichess-bot/
+ln -s /lichess-bot/config/config.yml /lichess-bot/
 
 if [ -e /lichess-bot/config/homemade.py ]; then
-    ln -sf /lichess-bot/config/homemade.py lichess-bot/
+    ln -sf /lichess-bot/config/homemade.py /lichess-bot/
 fi
 
 if [ -e /lichess-bot/config/extra_game_handlers.py ]; then
-    ln -sf /lichess-bot/config/extra_game_handlers.py lichess-bot/
+    ln -sf /lichess-bot/config/extra_game_handlers.py /lichess-bot/
 fi

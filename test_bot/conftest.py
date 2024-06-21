e3fd42a489f356b41a185dd004f1d13641046b19
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2024-06-21 07:40:46 +0300

    Add more tests (#982)
    Add some tests for external moves. A lot more should be added before #920 can be closed.
    Also fixes a bug in engine_wrapper found while creating these tests.

--- test_bot/conftest.py
+++ test_bot/conftest.py
@@ -7,6 +7,10 @@ from typing import Union
 
 
 def pytest_sessionfinish(session: Session, exitstatus: Union[int, ExitCode]) -> None:
-    """Remove files created when testing lichess-bot."""
+    """
+    Remove files created when testing lichess-bot.
+
+    The only exception is if running in a GitHub action, in which case we save the engines to the cache.
+    """
     if os.path.exists("TEMP") and not os.getenv("GITHUB_ACTIONS"):
         shutil.rmtree("TEMP")

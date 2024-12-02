87ac437dbde68a8d39bb706808f070fc2fbd4b79
Ioannis "Giannis" Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2024-12-02 21:06:10 +0100

    Add files via upload

---
+++ test_bot/test_model.py
@@ -0,0 +1,62 @@
+import datetime
+from lib import model
+import yaml
+from lib import config
+from collections import defaultdict
+
+
+def test_model():
+    challenge = {"id": "zzzzzzzz", "url": "https://lichess.org/zzzzzzzz", "status": "created",
+                 "challenger": {"id": "c", "name": "c", "rating": 2000, "title": None, "online": True},
+                 "destUser": {"id": "b", "name": "b", "rating": 3000, "title": "BOT", "online": True},
+                 "variant": {"key": "standard", "name": "Standard", "short": "Std"}, "rated": False, "speed": "bullet",
+                 "timeControl": {"type": "clock", "limit": 90, "increment": 1, "show": "1.5+1"}, "color": "random",
+                 "finalColor": "white", "perf": {"icon": "\ue032", "name": "Bullet"}}
+    user_profile = {"id": "b", "username": "b",
+                    "perfs": {"bullet": {"games": 100, "rating": 3000, "rd": 150, "prog": -10},
+                              "blitz": {"games": 100, "rating": 3000, "rd": 150, "prog": -10},
+                              "rapid": {"games": 100, "rating": 3000, "rd": 150, "prog": -10},
+                              "classical": {"games": 100, "rating": 3000, "rd": 150, "prog": -10},
+                              "correspondence": {"games": 100, "rating": 3000, "rd": 150, "prog": -10},
+                              "antichess": {"games": 100, "rating": 3000, "rd": 150, "prog": -10, "prov": True}},
+                    "title": "BOT", "createdAt": 1500000000000, "profile": {
+            "bio": "This is my bio",
+            "links": "https://github.com/lichess-bot-devs/lichess-bot"},
+                    "seenAt": 1700000000000, "playTime": {"total": 1000000, "tv": 10000},
+                    "url": "https://lichess.org/@/b",
+                    "count": {"all": 600, "rated": 500, "ai": 50, "draw": 200, "drawH": 50, "loss": 50,
+                              "lossH": 50, "win": 250, "winH": 200, "bookmark": 0, "playing": 0, "import": 0, "me": 0},
+                    "followable": True, "following": False, "blocking": False}
+    player1 = {"id": "c", "name": "c", "rating": 2000, "title": None, "online": True}
+    player2 = {"id": "b", "name": "b", "rating": 3000, "title": "BOT", "online": True}
+    game = {"id": "sAoJVsG3", "variant": {"key": "standard", "name": "Standard", "short": "Std"}, "speed": "bullet",
+            "perf": {"name": "Bullet"}, "rated": False, "createdAt": 1700000000000,
+            "white": {"id": "c", "name": "c", "title": None, "rating": 2000},
+            "black": {"id": "b", "name": "b", "title": "BOT", "rating": 3000},
+            "initialFen": "startpos", "clock": {"initial": 90000, "increment": 1000}, "type": "gameFull",
+            "state": {"type": "gameState", "moves": "", "wtime": 90000, "btime": 90000, "winc": 1000, "binc": 1000,
+                      "status": "started"}}
+    username = "b"
+    base_url = "https://lichess.org/"
+    abort_time = datetime.timedelta(seconds=30)
+    
+    with open("./config.yml.default") as file:
+        CONFIG = yaml.safe_load(file)
+    CONFIG["token"] = ""
+    CONFIG["challenge"]["allow_list"] = []
+    CONFIG["challenge"]["block_list"] = []
+    configuration = config.Configuration(CONFIG).challenge
+    recent_challenges = defaultdict()
+    recent_challenges["c"] = []
+    
+    challenge_model = model.Challenge(challenge, user_profile)
+    assert challenge_model.id == "zzzzzzzz"
+    assert challenge_model.rated is False
+    assert challenge_model.variant == "standard"
+    assert challenge_model.speed == "bullet"
+    assert challenge_model.time_control["show"] == "1.5+1"
+    assert challenge_model.color == "white"
+    assert challenge_model.is_supported(configuration, recent_challenges) == (True, "")
+
+    CONFIG["challenge"]["min_base"] = 120
+    assert challenge_model.is_supported(configuration, recent_challenges) == (False, "timeControl")

0b69252c31fae68c6b6c66a2384e355e1d31baea
Mark Harrison <MarkZH@users.noreply.github.com>
2025-10-05 04:43:29 -0700

    Make matchmaking challenge filters time-limited
    This will allow for filters to expire in case challenge declinations are
    for temporary reasons. Also, in the case of an opponent reaching the
    100-game daily limit, this will be useful for trying again in the time
    given in the interval.

--- lib/matchmaking.py
+++ lib/matchmaking.py
@@ -4,7 +4,7 @@ import logging
 import datetime
 import contextlib
 from lib import model
-from lib.timer import Timer, seconds, minutes, years
+from lib.timer import Timer, days, seconds, minutes, years
 from collections import defaultdict
 from collections.abc import Sequence
 from lib.lichess import Lichess, RateLimitedError
@@ -41,7 +41,7 @@ class Matchmaking:
         #   - variant (standard, horde, etc.)
         #   - casual/rated
         #   - empty string (if no other reason is given or self.filter_type is COARSE)
-        self.challenge_type_acceptable: defaultdict[tuple[str, str], bool] = defaultdict(lambda: True)
+        self.challenge_type_acceptable: defaultdict[tuple[str, str], Timer] = defaultdict(Timer)
         self.challenge_filter = self.matchmaking_cfg.challenge_filter
 
         for name in self.matchmaking_cfg.block_list:
@@ -101,7 +101,7 @@ class Matchmaking:
             if key == "bot.vsBot.day":
                 self.rate_limit_timer = Timer(seconds(float(rate_limit["seconds"])))
         else:
-            self.add_to_block_list(username)
+            self.add_challenge_filter(username, "")
         self.show_earliest_challenge_time()
 
 
@@ -263,22 +263,22 @@ class Matchmaking:
 
     def add_to_block_list(self, username: str) -> None:
         """Add a bot to the blocklist."""
-        self.add_challenge_filter(username, "")
+        self.add_challenge_filter(username, "", years(10))
 
     def in_block_list(self, username: str) -> bool:
         """Check if an opponent is in the block list to prevent future challenges."""
         return not self.should_accept_challenge(username, "")
 
-    def add_challenge_filter(self, username: str, game_aspect: str) -> None:
+    def add_challenge_filter(self, username: str, game_aspect: str, timeout: Union[datetime.timedelta, None] = None) -> None:
         """
-        Prevent creating another challenge when an opponent has decline a challenge.
+        Prevent creating another challenge for a timeout when an opponent has declined a challenge.
 
         :param username: The name of the opponent.
-        :param game_aspect: The aspect of a game (time control, chess variant, etc.)
-        that caused the opponent to decline a challenge. If the parameter is empty,
-        that is equivalent to adding the opponent to the block list.
+        :param game_aspect: The aspect of a game (time control, chess variant, etc.) that caused the opponent to decline a
+        challenge. If the parameter is empty, that is equivalent to adding the opponent to the block list.
+        :param timeout: The amount of time to not challenge an opponent. If None, the default is a day.
         """
-        self.challenge_type_acceptable[(username, game_aspect)] = False
+        self.challenge_type_acceptable[(username, game_aspect)] = Timer(timeout or days(1))
 
     def should_accept_challenge(self, username: str, game_aspect: str) -> bool:
         """
@@ -288,7 +288,7 @@ class Matchmaking:
         :param game_aspect: A category of the challenge type (time control, chess variant, etc.) to test for acceptance.
         If game_aspect is empty, this is equivalent to checking if the opponent is in the block list.
         """
-        return self.challenge_type_acceptable[(username, game_aspect)]
+        return self.challenge_type_acceptable[(username, game_aspect)].is_expired()
 
     def accepted_challenge(self, event: EventType) -> None:
         """
